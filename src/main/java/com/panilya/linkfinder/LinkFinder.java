511bdb3800b066ebcd6c9bf6ca0ff63378163897
panilya <panilyau@gmail.com>
2022-04-22 18:19:48 +0300

    Add ability to print if there is a link in all files in directory

--- src/main/java/com/panilya/linkfinder/LinkFinder.java
+++ src/main/java/com/panilya/linkfinder/LinkFinder.java
@@ -13,45 +13,60 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class LinkFinder {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String regexString = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)";
-        ExecutorService executorService = Executors.newFixedThreadPool(6);
-
         Pattern pattern = Pattern.compile(regexString);
-        String pathToFileTestingPurpose = "C:\\Users\\Bizzard28\\Dropbox\\Комп'ютер\\Documents\\programs\\tldr\\pages\\common\\r2.md";
+
+        String pathToFileWithoutLink = "C:\\Users\\Bizzard28\\Dropbox\\Комп'ютер\\Documents\\programs\\tldr\\pages\\common\\r2.md";
         String pathToSearch = "C:\\Users\\Bizzard28\\Dropbox\\Комп'ютер\\Documents\\programs\\tldr";
 
-//        Future<?> thread1 = executorService.submit(() -> {
-//            try {
-//                findRegex(pattern, pathToTestFile);
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
         long startTime = System.nanoTime();
-        System.out.println(listFilesInDir(Paths.get(pathToSearch)));
+
+        try {
+            System.out.println(listFilesInDir(Paths.get(pathToSearch)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<Boolean> list;
+        try {
+            list = listFilesInDir(Paths.get(pathToSearch)).stream()
+                    .filter(filePath -> !filePath.endsWith(".md"))
+                    .map(filePath -> {
+                        try {
+                            return findRegex(pattern, filePath);
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(list);
         long endTime = System.nanoTime();
         long timeElapsed = endTime - startTime;
-        System.out.println("Execution time: " + timeElapsed / 1000000);
+        System.out.println("Execution time: " + timeElapsed / 1000000000);
     }
 
-    private static boolean findRegex(Pattern pattern, String pathToTestFile) throws IOException {
+    private static boolean findRegex(Pattern pattern, Path path) throws IOException {
         boolean match;
-        try (Stream<String> filesStream = Files.lines(Paths.get(pathToTestFile))) {
+        try (Stream<String> filesStream = Files.lines(path)) {
             match = filesStream.map(input -> pattern.matcher(input).find())
                     .anyMatch(el -> el.equals(true));
         }
-        System.out.println(match);
-        System.out.println(Paths.get(pathToTestFile).getFileName() + " " + Paths.get(pathToTestFile).toRealPath());
+        System.out.println(match + " " + path.getFileName() + " " + path.toRealPath());
 
         return match;
     }
 
     private static List<Path> listFilesInDir(Path path) throws IOException {
         List<Path> pathList;
-        try (Stream<Path> walker = Files.walk(path, 15)) {
-            pathList = walker.filter(Files::isRegularFile)
-//                    .filter(file -> !file.toAbsolutePath().endsWith(".md"))
+        try (Stream<Path> walker = Files.walk(path)) {
+            pathList = walker.map(Path::normalize)
+                    .filter(Files::isRegularFile)
+                    .filter(file -> file.getFileName().toString().endsWith(".md"))
                     .collect(Collectors.toList());
         }
         return pathList;
